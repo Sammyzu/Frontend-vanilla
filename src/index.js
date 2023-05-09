313c34c88bb69e6e7bdedefe39348d12a64ed45b
Isu <jesusiza04@gmail.com>
2023-05-08 19:24:35 -0600

    interaccion de compra implementacion

--- src/index.js
+++ src/index.js
@@ -1,50 +1,74 @@
 import listaJuegos from "../static/videojuegos.json";
 
-const videojuegos = listaJuegos.map((juego) => {
-  return `<div class="juego">
-                <span class="titulo">
-                    ${juego.titulo}
-                </span>
-                <span class="consola">
-                    ${juego.consola}
-                </span>
-                <img src="${juego.imagen}" />
-                <p>${juego.descripcion}</p>
-                <em>$${juego.precio}</em>
-            </div>
-    `;
-});
+// Crear la lista de videojuegos en el DOM (div class="juego")
 
-const presupuesto = 300;
+const videojuegos = [];
 
-let juegosSeleccionados = 1;
+for (let i = 0; i < listaJuegos.length; i++) {
+  const contenedorJuegos = document.createElement("div");
+  contenedorJuegos.className = "juego";
+  const tituloJuego = document.createElement("span");
+  const consolaJuego = document.createElement("span");
+  const imagenJuego = document.createElement("img");
+  const descripcionJuego = document.createElement("p");
+  const precioJuego = document.createElement("em");
+  const checkboxJuego = document.createElement("input");
+  checkboxJuego.type = "checkbox";
+  checkboxJuego.name = "juego-seleccionado";
 
-let total = 60;
+  tituloJuego.className = "titulo";
+  consolaJuego.className = "consola";
+  tituloJuego.innerText = listaJuegos[i].titulo;
+  consolaJuego.innerText = listaJuegos[i].consola;
+  imagenJuego.src = listaJuegos[i].imagen;
+  descripcionJuego.innerText = listaJuegos[i].descripcion;
+  precioJuego.innerText = "$" + listaJuegos[i].precio;
 
-document.getElementById("presupuesto").innerText = "$" + presupuesto;
-
-document.getElementById("juegos-seleccionados").innerText = juegosSeleccionados;
-
-document.getElementById("total").innerText = "$" + total;
-
-function hacerCheckout() {
-  alert(
-    "presupuesto:" +
-      " " +
-      presupuesto +
-      "\n" +
-      "Cantidad de juegos:" +
-      " " +
-      juegosSeleccionados +
-      "\n" +
-      "total:" +
-      " " +
-      total
-  );
+  contenedorJuegos.appendChild(tituloJuego);
+  contenedorJuegos.appendChild(consolaJuego);
+  contenedorJuegos.appendChild(imagenJuego);
+  contenedorJuegos.appendChild(descripcionJuego);
+  contenedorJuegos.appendChild(precioJuego);
+  contenedorJuegos.appendChild(checkboxJuego);
+  videojuegos.push(contenedorJuegos);
 }
 
-document.getElementById("juegos").innerHTML = videojuegos.join("");
+const valorPresupuesto = 300;
+let valorJuegos = 0;
+let valorTotal = 60;
+
+const presupuesto = document.getElementById("presupuesto");
+const cantidadJuegos = document.getElementById("juegos-seleccionados");
+const total = document.getElementById("total");
+presupuesto.innerText = "$" + valorPresupuesto;
+cantidadJuegos.innerText = valorJuegos;
+total.innerText = "$" + valorTotal;
+
+function hacerCheckout() {}
+
+const juegos = document.getElementById("juegos");
+videojuegos.forEach((juego) => {
+  juegos.appendChild(juego);
+});
+
+// Listeners (Escuchadores?)
 
 const botonCheckout = document.querySelector(".checkout-btn");
-
 botonCheckout.addEventListener("click", hacerCheckout);
+
+const checkboxes = document.querySelectorAll(
+  "input[type=checkbox][name=juego-seleccionado]"
+);
+
+const seleccionarJuego = (event) => {
+  if (event.target.checked) {
+    valorJuegos += 1;
+  } else {
+    valorJuegos -= 1;
+  }
+  cantidadJuegos.innerText = valorJuegos;
+};
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", seleccionarJuego);
+});
